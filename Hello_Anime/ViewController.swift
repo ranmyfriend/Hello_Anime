86ab3550f0ae808f5ccf95eb0a61446b900d500d
ranjit <ranjithkumar2010a@live.com>
2017-09-19 00:38:35 +0530

    Initial Commit

---
+++ Hello_Anime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello_Anime
+//
+//  Created by Ranjith Kumar on 9/19/17.
+//  Copyright © 2017 Dash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
